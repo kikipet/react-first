9ca42991c0d3cc068856fd0408c021c711d05306
kikipet <songkimwl@gmail.com>
2021-03-06 00:20:48 -0500

    part 1 + some of part 2

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header() {
+    // equivalent to const Header = () => {}
+    return (
+        <header>
+            <h1>todos</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/TodoItem.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function TodoItem(props) {
+    // a class component that only has markup within the render() method can safely be converted to a function component
+    return (
+        <li>
+            <input 
+                type="checkbox" 
+                checked={props.todo.completed}
+                onChange={() => props.handleChangeProps(props.todo.id)}
+            />
+            {props.todo.title}
+        </li>
+    )
+}
+
+export default TodoItem
