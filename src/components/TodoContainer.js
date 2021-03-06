eeb827827d509f607d31812a828abbc2308df5b3
Song Kim <songkimwl@gmail.com>
2021-03-06 20:23:46 +0000

    part 3 - css

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -65,14 +65,16 @@ class TodoContainer extends React.Component {
     render() {
         return (
             // define various properties/props that can be accessed elsewhere
-            <div>
-                <Header />
-                <InputTodo addTodoProps={this.addTodoItem}/>
-                <TodosList 
-                    todos={this.state.todos} 
-                    handleChangeProps={this.handleChange}
-                    deleteTodoProps={this.delTodo}
-                />
+            <div className="container">
+				<div className="inner">
+					<Header />
+					<InputTodo addTodoProps={this.addTodoItem}/>
+					<TodosList 
+						todos={this.state.todos} 
+						handleChangeProps={this.handleChange}
+						deleteTodoProps={this.delTodo}
+					/>
+				</div>
             </div>
         );
     }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,30 @@
 import React from "react"
+import styles from "TodoItem.module.css"
 
 function TodoItem(props) {
     // a class component that only has markup within the render() method can safely be converted to a function component
+
+	const completeStyle = {
+		fontStyle: "italic",
+		color: "#595959",
+		opacity: 0.4,
+		textDecoration: "line-through"
+	}
+
     return (
-        <li>
+        <li className={styles.item}>
             <input 
                 type="checkbox" 
-                checked={props.todo.completed}
+                className={styles.checkbox}
+				checked={props.todo.completed}
                 onChange={() => props.handleChangeProps(props.todo.id)}
             />
             <button onClick={() => props.deleteTodoProps(props.todo.id)}>
                 Delete
             </button>
-            {props.todo.title}
+			<span style={props.todo.completed ? completedStyle : null}>
+            	{props.todo.title}
+			</span>
         </li>
     )
 }
